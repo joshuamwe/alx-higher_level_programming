00f58eea0077278ef42a169377a265a0d603898b
joshuamwe <joshuajomwema2819@gmail.com>
2023-11-28 15:04:04 +0300

    loop

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(10):
+for i in range(9):
     for j in range(i + 1, 10):
-        if i != j:
-            print("{:d}{:d}".format(i, j), end=", " if i < 8 or (i == 8 and j == 9) else "", flush=True)
-print()
+        if i * 10 + j < 89:
+            print("{:d}{:d}".format(i, j), end=", ")
+print("{:d}".format(89))
