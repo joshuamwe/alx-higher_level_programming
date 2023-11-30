7953fe032f469b4655f560a1ffc2c36b80ad1c27
joshuamwe <joshuajomwema2819@gmail.com>
2023-11-30 14:49:53 +0300

    jkl

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,10 +2,9 @@
 if __name__ == "__main__":
     import sys
 
-def add_arguments(*args):
-    result = sum(int(arg) for arg in args)
-    print(result)
+    length = len(sys.argv)
+    total = 0
 
-if __name__ == "__main__":
-    arguments = sys.argv[1:]
-    add_arguments(*arguments)
+    for i in range(1, length):
+        total += int(sys.argv[i])
+    print(f"{total:d}")
