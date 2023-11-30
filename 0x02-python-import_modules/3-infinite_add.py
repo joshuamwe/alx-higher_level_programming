71ab6d4dbc3122021105fb7a074098e1020a6d4b
joshuamwe <joshuajomwema2819@gmail.com>
2023-11-30 13:45:55 +0300

    kl

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+
+def add_arguments(*args):
+    result = sum(int(arg) for arg in args)
+    print(result)
+
+if __name__ == "__main__":
+    arguments = sys.argv[1:]
+    add_arguments(*arguments)
