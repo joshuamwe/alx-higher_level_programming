e7ae947f598c7db16f4465c9424b926be5b240ac
joshuamwe <joshuajomwema2819@gmail.com>
2023-11-30 14:45:29 +0300

    jkl

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-from add_0 as addition
-a = 1
-b = 2
+    import add_0 as addition
+    a = 1
+    b = 2
 
 print("{:d} + {:d} = {:d}".format(a, b, addition.add(a, b)))
