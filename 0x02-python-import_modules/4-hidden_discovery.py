23b9fb5dada5eb9b8d24a7a5d47100a8c0cf11d3
joshuamwe <joshuajomwema2819@gmail.com>
2023-11-30 14:15:47 +0300

    ghj

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import dis
-    import types
+    import hidden_4
 
-def print_module_names(module_path):
-    with open(module_path, 'rb') as file:
-        code = compile(file.read(), module_path, 'exec')
+    names = dir(hidden_4)
 
-    for instr in dis.get_instructions(code):
-        if instr.opname == 'LOAD_NAME' and not instr.argval.startswith('__'):
-            print(instr.argval)
-
-if __name__ == "__main__":
-    module_path = 'hidden_4.pyc'
-    print_module_names(module_path)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
