667ba5e1a005976d368637c25699fc47c01814dc
joshuamwe <joshuajomwema2819@gmail.com>
2023-12-02 11:45:19 +0300

    struct

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,11 +1,10 @@
-#!/usr/bin/pyhon3
+#!/usr/bin/python3
 def element_at(my_list, idx):
+
     n = len(my_list) - 1
     if idx < 0:
         return None
-
-    if idx >= len(my_list):
+    elif idx > n:
         return None
-
-    return my_list[idx]
-
+    else:
+        return my_list[idx]
