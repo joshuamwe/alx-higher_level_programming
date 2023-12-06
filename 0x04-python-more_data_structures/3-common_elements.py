4a003f1d7961e11f2ff44d991a585f6d9680b2e4
joshuamwe <joshuajomwema2819@gmail.com>
2023-12-06 15:33:25 +0300

    all

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
-def square_matrix_simple(matrix=[]):
+def common_elements(set_1, set_2):
 
-    return ([list(map(lambda x: x * x, row)) for row in matrix])
+    return (set_1 & set_2)
