cc031fb244bd67eae3f2e034abdbe6d3da9bd000
joshuamwe <joshuajomwema2819@gmail.com>
2024-03-13 22:53:58 +0300

    more sql

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- script that lists all priveleges of the MySql users
+-- for user_0d_1 and user_0d_2 on my server
+
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
